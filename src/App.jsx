24b70881124df02894ec2dd18bf4e30affc7f63a
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-09-24 13:40:24 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-//import Nwankwo from "./Components/Screens/Nwankwo";
 import Register from "./Components/Screens/Register";
 import Login from "./Components/Screens/Login";
-// import Main from "./Components/Screens/Main";
 import NwankwoHome from "./Components/Screens/NwankwoHome";
 import AsouzuHome from "./Components/Screens/AsouzuHome";
 import UdorjiHome from "./Components/Screens/UdorjiHome";
 import OkoliHome from "./Components/Screens/OkoliHome";
-// import Family4 from "./pages/Family4";
+
 
 function App() {
   return (
