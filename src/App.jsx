4127dce9b13e5c13618ecc7dd00bd0ebc5f45fb3
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-09-29 09:44:02 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           path="/home"
           element={
             <ProtectedRoute>
-              <Home />
+              <Hero />
             </ProtectedRoute>
           }
         />
