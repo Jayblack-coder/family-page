a82d7fadb54a2364449148184f44cde091bcf072
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-10-01 15:06:18 +0100

    nwankwofirst

--- src/Components/Screens/NwankwoFirst.jsx
+++ src/Components/Screens/NwankwoFirst.jsx
@@ -37,7 +37,7 @@ const NwankwoGenOneProfiles = () => {
   useEffect(() => {
     const newFilteredData = originalData.filter(
      (item) => item.surname === "Nwankwo" &&
-        Number(item.generation) === 4
+        String(item.generation) === "4th"
     );
     setFilteredData(newFilteredData);
   }, [originalData]);
