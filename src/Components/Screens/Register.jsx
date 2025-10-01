fed819013048f318b9cf1d04005a65b818c1f3cb
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-10-01 20:39:18 +0100

    register

--- src/Components/Screens/Register.jsx
+++ src/Components/Screens/Register.jsx
@@ -473,7 +473,7 @@ const Register = () => {
           <MenuItem value="2nd">2nd</MenuItem>
           <MenuItem value="3rd">3rd</MenuItem>
           <MenuItem value="4th">4th</MenuItem>
-          <MenuItem value="4th">5th</MenuItem>
+          <MenuItem value="5th">5th</MenuItem>
         </TextField>
        <LocalizationProvider dateAdapter={AdapterDayjs}>
   <DatePicker
