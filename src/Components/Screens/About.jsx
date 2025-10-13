c0fe1ee962ea8afcdf6b1b4ac691a05043acfacc
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-10-13 08:31:00 +0100

    about

--- src/Components/Screens/About.jsx
+++ src/Components/Screens/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       <h1>About Us</h1>
       <div>
-        <p>The Nmelonye family is a nigerian family whose history has been chronicled in a book titled, From Nise to Arondizuogu to the world: Nmelonye family history 1819-2004. The genealogy traces the family's history from 1819 to 2004 and includes information on their lineage from Nise in Anambra state to Arondizuogu in Imo state, with the book published by Cheedal Global Prints. The history details the life and times of Nmelonye as their ancestor who as a native of Nise, migrated to Arondizuogu due to family disputes. He then settled in his new found home, married and vgave birth to five offsprings from whom the entire 4 families of Nmelonye dynasty emerged. The offsprings are: </p>
+        <p>The Nmelonye family is a nigerian family whose history has been chronicled in a book titled, From Nise to Arondizuogu to the world: Nmelonye family history 1819-2004. The genealogy traces the family's history from 1819 to 2004 and includes information on their lineage from Nise in Anambra state to Arondizuogu in Imo state, with the book published by Cheedal Global Prints. The history details the life and times of Nmelonye as their ancestor who as a native of Nise, migrated to Arondizuogu due to family disputes. He then settled in his new found home, married and gave birth to five offsprings from whom the entire 4 families of Nmelonye dynasty emerged. The offsprings are: </p>
 <ul>
   <li>Agosi Nmelonye who died in battle in his youth, without any offspring</li>
   <li>Nwankwo Nmelonye</li>
