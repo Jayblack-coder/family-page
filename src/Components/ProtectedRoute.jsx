18e79979125e6d4edfb67e1f8c1a62f333f62a2f
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-11-13 15:18:11 +0000

    protectedroute

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,30 +1,30 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx"; 
 
-const ProtectedRoute = ({ children, adminOnly = false }) => {
-  const { user, token } = useAuth();
+// const ProtectedRoute = ({ children, adminOnly = false }) => {
+//   const { user, token } = useAuth();
 
-  if (!token || !user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (adminOnly && user.familyStatus.toLowerCase() !== "admin") {
-    return <Navigate to="/" replace />;
-  }
-
-  return children;
-};
-
-// const ProtectedRoute = ({ children }) => {
-//   const token = localStorage.getItem("token"); // check if user is logged in
-
-//   if (!token) {
-//     // 🚫 not logged in → redirect to login
+//   if (!token || !user) {
 //     return <Navigate to="/login" replace />;
 //   }
 
-//   // ✅ logged in → show page
+//   if (adminOnly && user.familyStatus.toLowerCase() !== "admin") {
+//     return <Navigate to="/" replace />;
+//   }
+
 //   return children;
 // };
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token"); // check if user is logged in
+
+  if (!token) {
+    // 🚫 not logged in → redirect to login
+    return <Navigate to="/login" replace />;
+  }
+
+  // ✅ logged in → show page
+  return children;
+};
 
 export default ProtectedRoute;
