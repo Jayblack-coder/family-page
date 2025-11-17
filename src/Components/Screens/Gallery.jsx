0555b51db2b3be863759cb48e1f523d56c958bfa
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-11-17 10:27:03 +0000

    gallery

--- src/Components/Screens/Gallery.jsx
+++ src/Components/Screens/Gallery.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Container, Typography, Box } from "@mui/material";
+import GalleryUpload from "../Admin/GalleryUpload"; // <-- make sure this path is correct
 
 const Gallery = () => {
   return (
-    <div>
-      <h1>Family Gallery</h1>
-    </div>
-  )
-}
+    <Box sx={{ bgcolor: "#f5f5f5", minHeight: "100vh", py: 5 }}>
+      <Container maxWidth="md">
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          textAlign="center"
+          gutterBottom
+          sx={{ color: "#004aad", mb: 3 }}
+        >
+          Family Event Gallery
+        </Typography>
 
-export default Gallery
+        {/* Reuse the upload component */}
+        <GalleryUpload />
+      </Container>
+    </Box>
+  );
+};
+
+export default Gallery;
