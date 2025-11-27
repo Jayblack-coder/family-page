94ac47664a7d481b42aeb41cee7a459bb6e4f03d
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-11-27 11:05:58 +0000

    events

--- src/Components/Screens/Events.jsx
+++ src/Components/Screens/Events.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import API from "../Screens/api.jsx";
 
 const Events = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    API.get("/api/events").then(res => setEvents(res.data));
+  }, []);
+
   return (
     <div>
       <h1>Family Events</h1>
+      {events.map(evt => (
+        <div key={evt._id}>
+          <h3>{evt.title}</h3>
+          <p>{evt.date}</p>
+          <p>{evt.description}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
