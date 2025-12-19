1fc9b433827257a736be1e29ff64796166544c4b
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2025-12-19 16:21:44 +0000

    galleryupload

--- src/Components/Admin/GalleryUpload.jsx
+++ src/Components/Admin/GalleryUpload.jsx
@@ -77,6 +77,10 @@ const GalleryUpload = () => {
   const { token } = useAuth();
 
   const uploadImage = async () => {
+    if (!token) {
+    toast.error("Session expired. Please login again.");
+    return;
+  }
     if (!file) return toast.error("Please choose an image");
 
     const formData = new FormData();
